76e6941b93cb543258c78a0eb5871b024c1ffece
Queras <142896487+Queras@users.noreply.github.com>
2024-02-03 00:15:02 +0100

    UI
    Added UI elements. Sett up hotbar and scripted how to move items.

--- Assets/Scenes/Henrik/Scripts/openCrafting.cs
+++ Assets/Scenes/Henrik/Scripts/openCrafting.cs
@@ -7,6 +7,8 @@ public class openCrafting : MonoBehaviour
     public GameObject craftingGrid;
     public GameObject mix;
     public GameObject camera;
+    public GameObject Slot1;
+    public GameObject Slot2;
     private MouseCamLook mouseCamLook;
 
     void Start()
@@ -14,6 +16,8 @@ public class openCrafting : MonoBehaviour
         caldron.SetActive(false);
         craftingGrid.SetActive(false);
         mix.SetActive(false);
+        Slot1.SetActive(false);
+        Slot2.SetActive(false);
         mouseCamLook = camera.GetComponent<MouseCamLook>();
     }
 
@@ -27,6 +31,8 @@ public class openCrafting : MonoBehaviour
             caldron.SetActive(state);
             craftingGrid.SetActive(state);
             mix.SetActive(state);
+            Slot1.SetActive(state);
+            Slot2.SetActive(state);
             if (state)
             {
                 Cursor.lockState = CursorLockMode.None;
