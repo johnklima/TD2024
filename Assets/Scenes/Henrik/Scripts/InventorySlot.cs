f1fec63fcac378c9b60c3c6e52bd2b28b2b002eb
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2024-02-13 18:46:57 +0100

    temp inventory methods

--- Assets/Scenes/Henrik/Scripts/InventorySlot.cs
+++ Assets/Scenes/Henrik/Scripts/InventorySlot.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public bool isCraftSlot;
-
+    public BaseItem Item { get; private set; }
+    public Image slotImage;
+    public Text quantityText;
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
@@ -16,6 +19,27 @@ public class InventorySlot : MonoBehaviour, IDropHandler
             {
                 // check if recipe is good, behave accordingly
             }
+        }
+    }
+
+    public void SetItem(BaseItem item, int quantity)
+    {
+        Item = item;
+        slotImage.sprite = item.uiSprite;
+        slotImage.enabled = true;
+        if (quantityText != null)
+        {
+            quantityText.text = quantity.ToString();
+
+        }
+    }
+
+    public void UpdateQuantity(int newQuantity)
+    {
+        if (quantityText != null)
+        {
+            quantityText.text = newQuantity.ToString();
+
         }
     }
 }
