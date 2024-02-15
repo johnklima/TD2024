38b62ee49b3c4ad63550e44a658f99c5f1d7187c
OikoumE <83828323+OikoumE@users.noreply.github.com>
2024-02-15 20:27:21 +0100

    made a Static (global) CursorLockHandler

--- Assets/Scenes/Leif/Scripts/CursorLockHandler.cs
+++ Assets/Scenes/Leif/Scripts/CursorLockHandler.cs
@@ -1,26 +1,42 @@
+using UnityEngine;
+
 public static class CursorLockHandler
 {
+    public static CursorLockMode GetCursorLockMode()
+    {
+        return Cursor.lockState;
+    }
+
+
     public static void LockCursor()
     {
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public static void UnLockCursor()
     {
+        Cursor.lockState = CursorLockMode.None;
     }
 
-    public static void HideLockCursor()
+    public static void HideCursor()
     {
+        Cursor.visible = false;
     }
 
-    public static void LockAndHideLockCursor()
+    public static void HideAndLockCursor()
     {
+        LockCursor();
+        HideCursor();
     }
 
-    public static void ShowLockCursor()
+    public static void ShowCursor()
     {
+        Cursor.visible = true;
     }
 
-    public static void UnlockAndShowLockCursor()
+    public static void ShowAndUnlockCursor()
     {
+        UnLockCursor();
+        ShowCursor();
     }
 }
