485eeb666165cc2e6e5d065cfdc0919a4a5dab60
OikoumE <83828323+OikoumE@users.noreply.github.com>
2024-02-15 20:12:31 +0100

    adding cursorhandler

---
+++ Assets/Scenes/Leif/Scripts/CursorLockHandler.cs
@@ -0,0 +1,26 @@
+public static class CursorLockHandler
+{
+    public static void LockCursor()
+    {
+    }
+
+    public static void UnLockCursor()
+    {
+    }
+
+    public static void HideLockCursor()
+    {
+    }
+
+    public static void LockAndHideLockCursor()
+    {
+    }
+
+    public static void ShowLockCursor()
+    {
+    }
+
+    public static void UnlockAndShowLockCursor()
+    {
+    }
+}
--- Assets/Scenes/Leif/WarningMe.cs
+++ Assets/Scenes/Leif/WarningMe.cs
@@ -18,7 +18,8 @@ public class WarningMe : MonoBehaviour
         }
 
         Debug.Log(msg("<b>Please</b> do not change anything in this scene!"));
-        Debug.Log(msg("If you want to modify this scene in any way \nmake a copy of it and put in your folder!"));
+        Debug.Log(msg("If you want to modify this scene in any way:"));
+        Debug.Log(msg("make a copy of it and put in your folder!"));
         Debug.Log(msg("</color><color=green>Thank you!</color> - <color=white><b>Leif</b>"));
     }
 }
