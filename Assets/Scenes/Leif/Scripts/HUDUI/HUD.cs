c8435ac0060ed0a8e1f447de07ac5922ed57630d
OikoumE <83828323+OikoumE@users.noreply.github.com>
2024-03-01 23:30:48 +0100

    alot of reorganizing and fixed  fishpole weird wobble

--- Assets/Scenes/Leif/Scripts/HUDUI/HUD.cs
+++ Assets/Scenes/Leif/Scripts/HUDUI/HUD.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class HUD : MonoBehaviour
 {
     public UnityEvent OnUiButtonClicked;
-    public AudioClip buttonPressAudioClip;
+    [Range(0, 1f)] public float volume = .5f;
+    public AudioClip buttonPressAudioClip, hotBarItemDragAudioClip;
     private AudioSource _audioSource;
 
     private void Start()
@@ -15,20 +16,29 @@ public class HUD : MonoBehaviour
 
         _audioSource = gameObject.AddComponent<AudioSource>();
         _audioSource.playOnAwake = false;
-        _audioSource.clip = buttonPressAudioClip;
+        _audioSource.volume = volume;
+
         var buttons = FindObjectsOfType<Button>(true);
 
         foreach (var button in buttons) button.onClick.AddListener(ButtonClick);
     }
 
-    // Update is called once per frame
-    private void Update()
-    {
-    }
-
     private void ButtonClick()
     {
+        PlaySound(buttonPressAudioClip);
         OnUiButtonClicked.Invoke();
+    }
+
+    public void ItemDragged()
+    {
+        PlaySound(hotBarItemDragAudioClip);
+        OnUiButtonClicked.Invoke();
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip == null) throw new Exception(clip + " clip not found");
+        _audioSource.clip = clip;
         _audioSource.Play();
     }
 }
